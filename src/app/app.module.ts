4aae5bfcac0a461ed1de8c167af437ee06ee0394
pawan <pawanyadavsultania@gmail.com>
2018-03-02 17:50:46 +0530

    recipe item need to be displayed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,10 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent} from './header/header.component';
-import { RecepiesComponent } from './recepies/recepies.component';
-import { RecipeListComponent } from './recepies/recipe-list/recipe-list.component';
-import { RecipeDetailComponent } from './recepies/recipe-detail/recipe-detail.component';
-import { RecipeItemComponent } from './recepies/recipe-list/recipe-item/recipe-item.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 
@@ -15,7 +15,7 @@ import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-ed
   declarations: [
     AppComponent,
     HeaderComponent,
-    RecepiesComponent,
+    RecipesComponent,
     RecipeListComponent,
     RecipeDetailComponent,
     RecipeItemComponent,
